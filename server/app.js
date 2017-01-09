1193b755af386b2f7f3fc42411c91b14b0381158
Deforis Nash <defhnash@gmail.com>
2017-01-09 09:30:35 -0600

    ports edited

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose'); // punch in requirements
 var route = require('./routes/route.js');
+var port = process.env.PORT || 1337;
 app.use(bodyParser.urlencoded({
     extended: true
 }));
@@ -12,8 +13,8 @@ app.use(bodyParser.json());
 var mongoBase = "mongodb://localhost:27017/PetsProject"; //assigns the URL(?), of the mongo Databae to a var
 var DBconnect = mongoose.connect(mongoBase).connection; //connects to that DB
 
-app.listen(1337, function() {
-    console.log("listening on that leet port");
+app.listen(port, function(req, res) {
+    console.log("listening on that leet port", port);
 }); //server listening
 
 
